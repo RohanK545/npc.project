b9674cd2c6a06ca4904a13cb02818dc9553b1aaf
vishvesh5 <vishvesh.tech555@gmail.com>
2025-12-24 11:10:51 +0530

    Changes in archive page.

--- src/app/keyLinks/archive/page.tsx
+++ src/app/keyLinks/archive/page.tsx
@@ -10,18 +10,7 @@ export default function AboutUs() {
         navItems={navigationConfig["keyLinks"].navItems}
         pageTilte="Archive"
       />
-      <h1 className="mt-5">This is Ministry Page</h1>
-      <div style={{ marginTop: "2rem" }}>
-        <h2>Test Content</h2>
-        <p>If the header is working correctly, you should see:</p>
-        <ul>
-          <li>Government of India branding</li>
-          <li>Ministry name</li>
-          <li>Skill India logo</li>
-          <li>Navigation menu</li>
-          <li>Search bar</li>
-        </ul>
-      </div>
+      
     </div>
   );
 }
